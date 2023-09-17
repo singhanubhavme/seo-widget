116051085b9618fa86501d4d013d8e07c3c59a1d
Anubhav Singh <singhanubhav58@gmail.com>
2023-09-17 11:24:11 +0530

    changes

--- src/app/(site)/[...site]/page.tsx
+++ src/app/(site)/[...site]/page.tsx
@@ -1,21 +1,19 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 'use client';
 import { useState, useEffect } from 'react';
-import axios from 'axios';
 import LoadingImg from '../../loading-2.svg';
 import Image from 'next/image';
 import CircularProgressBar from '@/components/CircularProgressBar';
 import SimpleCard from '@/components/SimpleCard';
 import Card from '@/components/Card';
+import {
+  getId,
+  getPageData,
+  getResources,
+  getScreenshot,
+  convertToHttpOrHttps,
+} from '../../../utils/data';
 export default function Home({ params }: { params: { site: Array<string> } }) {
-  const convertToHttpOrHttps = (url: string) => {
-    return url.startsWith('http://') ||
-      url.startsWith('https://') ||
-      url.startsWith('https%3A')
-      ? url
-      : `https://${url}`;
-  };
-
   const getRandom = (min: number, max: number): number =>
     Math.floor(Math.random() * (max - min)) + min;
 
@@ -30,158 +28,24 @@ export default function Home({ params }: { params: { site: Array<string> } }) {
   const [seoDetails, setSeoDetails]: any = useState({});
   const [errCount, setErrCount] = useState<number>(0);
 
-  async function getId() {
-    const post_array: any = [];
-    post_array.push({
-      target: URL,
-      max_crawl_pages: 10,
-      load_resources: true,
-      enable_javascript: true,
-      enable_browser_rendering: true,
-    });
-    const respose = await axios({
-      method: 'post',
-      url: 'https://api.dataforseo.com/v3/on_page/task_post',
-      data: post_array,
-      headers: {
-        Authorization: process.env.NEXT_PUBLIC_AUTH,
-      },
-    });
-    setId(respose?.data?.tasks[0]?.id);
-  }
-
-  async function getPageData() {
-    const id = localStorage.getItem('id');
-    const post_array = [];
-    post_array.push({
-      id: id,
-    });
-    const response = await axios.post(
-      'https://api.dataforseo.com/v3/on_page/pages',
-      post_array,
-      {
-        headers: {
-          Authorization: process.env.NEXT_PUBLIC_AUTH,
-        },
-      }
-    );
-    let countBrokenLinks = 0,
-      countBrokenResources = 0,
-      countDuplicateContent = 0,
-      countDuplicateTitle = 0,
-      countDuplicateDesciption = 0;
-    response?.data?.['tasks']?.[0]?.['result']?.[0]?.['items']?.map(
-      (item: any) => {
-        if (item.broken_links) {
-          countBrokenLinks++;
-          setErrCount((errCount) => errCount + 1);
-        }
-        if (item.broken_resources) {
-          countBrokenResources++;
-          setErrCount((errCount) => errCount + 1);
-        }
-        if (item.duplicate_content) {
-          countDuplicateContent++;
-        }
-        if (item.duplicate_description) {
-          countDuplicateDesciption++;
-        }
-        if (item.duplicate_title) {
-          countDuplicateTitle++;
-        }
-      }
-    );
-    setSeoDetails({
-      ...seoDetails,
-      countBrokenLinks,
-      countBrokenResources,
-      countDuplicateContent,
-      countDuplicateTitle,
-      countDuplicateDesciption,
-    });
-    return response?.data?.['tasks']?.[0]?.['result']?.[0]?.['crawl_progress'];
-  }
-
-  async function getResources() {
-    const id = localStorage.getItem('id');
-    const post_array = [];
-    post_array.push({
-      id: id,
-    });
-    const response = await axios.post(
-      'https://api.dataforseo.com/v3/on_page/resources',
-      post_array,
-      {
-        headers: {
-          Authorization: process.env.NEXT_PUBLIC_AUTH,
-        },
-      }
-    );
-    let countImg = 0,
-      countScript = 0,
-      countStylesheet = 0,
-      totalImgSize = 0,
-      totalScriptSize = 0,
-      totalStylesheetSize = 0;
-    response?.data?.['tasks']?.[0]?.['result']?.[0]?.['items']?.map(
-      (item: any) => {
-        if (item.resource_type === 'image') {
-          countImg++;
-          totalImgSize += item.size;
-        }
-        if (item.resource_type === 'script') {
-          countScript++;
-          totalScriptSize += item.size;
-        }
-        if (item.resource_type === 'stylesheet') {
-          countStylesheet++;
-          totalStylesheetSize += item.size;
-        }
-      }
-    );
-    setOnPageResources({
-      ...onPageResources,
-      'Number of Images': countImg,
-      Scripts: countScript,
-      'Number of Stylesheets': countStylesheet,
-      'Images Size': totalImgSize,
-      'Scripts Size': totalScriptSize,
-      'Stylesheets Size': totalStylesheetSize,
-    });
-    return response?.data?.['tasks']?.[0]?.['result']?.[0]?.['crawl_progress'];
-  }
-
-  async function getScreenshot() {
-    const post_array = [];
-    post_array.push({
-      url: URL,
-      full_page_screenshot: false,
-    });
-    const response = await axios.post(
-      'https://api.dataforseo.com/v3/on_page/page_screenshot',
-      post_array,
-      {
-        headers: {
-          Authorization: process.env.NEXT_PUBLIC_AUTH,
-        },
-      }
-    );
-    setSS(response?.data?.tasks?.[0]?.result?.[0]?.items?.[0]?.image);
-  }
-
   useEffect(() => {
     let intervalId1: any, intervalId2: any;
     if (id) {
       localStorage.setItem('id', id);
-      getScreenshot();
+      getScreenshot(setSS);
       intervalId1 = setInterval(async () => {
-        const progress1 = getResources();
+        const progress1 = getResources(onPageResources, setOnPageResources);
         if ((await progress1) === 'finished') {
           clearInterval(intervalId1);
         }
       }, 2000);
       intervalId2 = setInterval(async () => {
-        const progress2 = getPageData();
+        const progress2 = getPageData(
+          setErrCount,
+          errCount,
+          setSeoDetails,
+          seoDetails
+        );
         if ((await progress2) === 'finished') {
           setLoadingCard(false);
           clearInterval(intervalId2);
@@ -196,7 +60,7 @@ export default function Home({ params }: { params: { site: Array<string> } }) {
 
   useEffect(() => {
     if (URL) {
-      getId();
+      getId(setId);
       setLoading(false);
     }
   }, [URL]);
---
+++ src/utils/data.ts
@@ -0,0 +1,155 @@
+import axios from 'axios';
+export async function getId(setId: (id: string | null) => void) {
+  const post_array: any = [];
+  post_array.push({
+    target: URL,
+    max_crawl_pages: 10,
+    load_resources: true,
+    enable_javascript: true,
+    enable_browser_rendering: true,
+  });
+  const respose = await axios({
+    method: 'post',
+    url: 'https://api.dataforseo.com/v3/on_page/task_post',
+    data: post_array,
+    headers: {
+      Authorization: process.env.NEXT_PUBLIC_AUTH,
+    },
+  });
+  setId(respose?.data?.tasks[0]?.id);
+}
+
+export async function getPageData(
+  setErrCount: (errCount: number) => void,
+  errCount: number,
+  setSeoDetails: (details: any) => void,
+  seoDetails: any
+) {
+  const id = localStorage.getItem('id');
+  const post_array = [];
+  post_array.push({
+    id: id,
+  });
+  const response = await axios.post(
+    'https://api.dataforseo.com/v3/on_page/pages',
+    post_array,
+    {
+      headers: {
+        Authorization: process.env.NEXT_PUBLIC_AUTH,
+      },
+    }
+  );
+  let countBrokenLinks = 0,
+    countBrokenResources = 0,
+    countDuplicateContent = 0,
+    countDuplicateTitle = 0,
+    countDuplicateDesciption = 0;
+  response?.data?.['tasks']?.[0]?.['result']?.[0]?.['items']?.map(
+    (item: any) => {
+      if (item.broken_links) {
+        countBrokenLinks++;
+        setErrCount((errCount) => errCount + 1);
+      }
+      if (item.broken_resources) {
+        countBrokenResources++;
+        setErrCount((errCount) => errCount + 1);
+      }
+      if (item.duplicate_content) {
+        countDuplicateContent++;
+      }
+      if (item.duplicate_description) {
+        countDuplicateDesciption++;
+      }
+      if (item.duplicate_title) {
+        countDuplicateTitle++;
+      }
+    }
+  );
+  setSeoDetails({
+    ...seoDetails,
+    countBrokenLinks,
+    countBrokenResources,
+    countDuplicateContent,
+    countDuplicateTitle,
+    countDuplicateDesciption,
+  });
+  return response?.data?.['tasks']?.[0]?.['result']?.[0]?.['crawl_progress'];
+}
+
+export async function getResources(
+  onPageResources: any,
+  setOnPageResources: (resources: any) => void
+) {
+  const id = localStorage.getItem('id');
+  const post_array = [];
+  post_array.push({
+    id: id,
+  });
+  const response = await axios.post(
+    'https://api.dataforseo.com/v3/on_page/resources',
+    post_array,
+    {
+      headers: {
+        Authorization: process.env.NEXT_PUBLIC_AUTH,
+      },
+    }
+  );
+  let countImg = 0,
+    countScript = 0,
+    countStylesheet = 0,
+    totalImgSize = 0,
+    totalScriptSize = 0,
+    totalStylesheetSize = 0;
+  response?.data?.['tasks']?.[0]?.['result']?.[0]?.['items']?.map(
+    (item: any) => {
+      if (item.resource_type === 'image') {
+        countImg++;
+        totalImgSize += item.size;
+      }
+      if (item.resource_type === 'script') {
+        countScript++;
+        totalScriptSize += item.size;
+      }
+      if (item.resource_type === 'stylesheet') {
+        countStylesheet++;
+        totalStylesheetSize += item.size;
+      }
+    }
+  );
+  setOnPageResources({
+    ...onPageResources,
+    'Number of Images': countImg,
+    Scripts: countScript,
+    'Number of Stylesheets': countStylesheet,
+    'Images Size': totalImgSize,
+    'Scripts Size': totalScriptSize,
+    'Stylesheets Size': totalStylesheetSize,
+  });
+  return response?.data?.['tasks']?.[0]?.['result']?.[0]?.['crawl_progress'];
+}
+
+export async function getScreenshot(setSS: (image: string | null) => void) {
+  const post_array = [];
+  post_array.push({
+    url: URL,
+    full_page_screenshot: false,
+  });
+  const response = await axios.post(
+    'https://api.dataforseo.com/v3/on_page/page_screenshot',
+    post_array,
+    {
+      headers: {
+        Authorization: process.env.NEXT_PUBLIC_AUTH,
+      },
+    }
+  );
+  setSS(response?.data?.tasks?.[0]?.result?.[0]?.items?.[0]?.image);
+}
+
+export function convertToHttpOrHttps(url: string) {
+  return url.startsWith('http://') ||
+    url.startsWith('https://') ||
+    url.startsWith('https%3A')
+    ? url
+    : `https://${url}`;
+}
